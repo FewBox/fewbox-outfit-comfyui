ebdbea1d9679bd81736700eefab117e04020fd43
XL <xl@fewbox.com>
2025-01-31 17:52:46 +0800

    Refine code.

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from .nodes import FewBoxOutfit
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+
+NODE_CLASS_MAPPINGS = {
+    "FewBoxOutfit": FewBoxOutfit
+}
+ 
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "FewBoxOutfit": "📦 Outfit"
+}
+
+WEB_DIRECTORY = "./js"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
---
+++ nodes.py
@@ -0,0 +1,24 @@
+class FewBoxOutfit:
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+            
+            
+            },
+        }
+ 
+    RETURN_TYPES = ()
+    RETURN_NAMES = ()
+ 
+    FUNCTION = "test"
+ 
+    #OUTPUT_NODE = False
+ 
+    CATEGORY = "📦 FewBox"
+ 
+    def test(self):
+        return ()
